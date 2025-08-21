688f4f06afd3143b4e9c3d9deb746771bc987950
benjamin <2000beni770@gmail.com>
2025-08-21 14:17:51 +0300

    eneter all processor func to calss

--- app/processor.py
+++ app/processor.py
@@ -4,63 +4,67 @@ from tqdm.auto import tqdm
 import pandas as pd
 from config import blacklist_path
 
+class Processor:
+    def _count_words(self, tweet):
+        words = tweet.split()
+        count = {}
+        for word in words:
+            if word in count:
+                count[word] += 1
+            else:
+                count[word] = 1
+        return count
 
-def _count_words(tweet):
-    words = tweet.split()
-    count = {}
-    for word in words:
-        if word in count:
-            count[word] += 1
-        else:
-            count[word] = 1
-    return count
+    def _find_rarest_world(self, tweet):
+        my_dict = self._count_words(tweet)
+        return min(my_dict, key=my_dict.get)
 
-def _find_rarest_world(tweet):
-    my_dict = _count_words(tweet)
-    return min(my_dict, key=my_dict.get)
+    def assign_rarest_world(self,df):
+        text_col = df["Text"]
+        tqdm.pandas()
+        df['rarest_world'] =  text_col.progress_apply(self._find_rarest_world)
 
-def assign_rarest_world(df):
-    text_col = df["Text"]
-    tqdm.pandas()
-    df['rarest_world'] =  text_col.progress_apply(_find_rarest_world)
+    def _classified_emotion(self, tweet):
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+        return score["compound"]
 
-def _classified_emotion(tweet):
-    score = SentimentIntensityAnalyzer().polarity_scores(tweet)
-    return score["compound"]
-
-def assign_emotion(df):
-    text_col = df["Text"]
-    tqdm.pandas()
-    scores = text_col.progress_apply(_classified_emotion) # pandas apply
-    bins = [-1, -0.5, 0.5, 1]
-    labels = ["negative", "neutral", "positive"]
-    df['sentiment'] = pd.cut(scores, bins=bins, labels=labels, right=True)
-
-
-def _load_blacklist():
-    black_list = []
-    with open(blacklist_path, "r") as file:
-        for line in file:
-            black_list.append(line.rstrip())
-    return black_list
+    def assign_emotion(self, df):
+        text_col = df["Text"]
+        tqdm.pandas()
+        scores = text_col.progress_apply(self._classified_emotion) # pandas apply
+        bins = [-1, -0.5, 0.5, 1]
+        labels = ["negative", "neutral", "positive"]
+        df['sentiment'] = pd.cut(scores, bins=bins, labels=labels, right=True)
 
 
-def _detected_weapons(tweet,black_list):
-    words = tweet.split()
-    for word in words:
-        if word in black_list:
-            return word
-    return ""
+    def _load_blacklist(self):
+        black_list = []
+        with open(blacklist_path, "r") as file:
+            for line in file:
+                black_list.append(line.rstrip())
+        return black_list
 
-def assign_weapons_detected(df):
-    black_list = _load_blacklist()
-    text_col = df["Text"]
-    tqdm.pandas()
-    df['weapons_detected'] = text_col.progress_apply(_detected_weapons, args=(black_list,))
-    print(df['weapons_detected'])
+    # ----- iterating over the black list
+    # ----- to check if tween contains this line - one word or more
+    def _detected_weapons(self, tweet,black_list):
+        for weapon in black_list:
+            if weapon in tweet:
+                return weapon
+        return ""
 
-# df = Fetcher.get_df()
-# assign_weapons_detected(df)
-# assign_rarest_world(df)
-# assign_emotion(df)
-# print(df)
+    def assign_weapons_detected(self, df):
+        black_list = self._load_blacklist()
+        text_col = df["Text"]
+        tqdm.pandas()
+        df['weapons_detected'] = text_col.progress_apply(self._detected_weapons, args=(black_list,))
+
+
+df = Fetcher.get_df()
+pr = Processor()
+pr.assign_weapons_detected(df)
+pr.assign_rarest_world(df)
+pr.assign_emotion(df)
+pr.assign_weapons_detected(df)
+
+print(df.loc[5])
+
