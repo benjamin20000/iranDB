93ee24c2e37c51bfad0e05027eb541b80ae8f954
benjamin <2000beni770@gmail.com>
2025-08-21 15:00:57 +0300

    handel problem inconection

--- app/fetcher.py
+++ app/fetcher.py
@@ -2,12 +2,13 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 import pandas as pd
+from app.config import env_path
 
 
 class Fetcher:
     @staticmethod
     def _get_uri():
-        load_dotenv(dotenv_path=".env")
+        load_dotenv(dotenv_path=env_path)
         username = os.getenv("username")
         password = os.getenv("password")
         db = os.getenv("db_name")
@@ -16,16 +17,21 @@ class Fetcher:
 
     @staticmethod
     def _get_tweets():
-        uri = Fetcher._get_uri()
-        client = MongoClient(uri)
-        mydb = client["IranMalDB"]
-        collection  = mydb["tweets"]
-        tweets_collection = collection.find({},{"_id":0})
-        tweets_arr = []
-        for tweet in tweets_collection:
-            tweets_arr.append(tweet)
-        client.close()
-        return tweets_arr
+        try:
+            uri = Fetcher._get_uri()
+            client = MongoClient(uri)
+            mydb = client["IranMalDB"]
+            collection  = mydb["tweets"]
+            tweets_collection = collection.find({},{"_id":0})
+            tweets_arr = []
+            for tweet in tweets_collection:
+                tweets_arr.append(tweet)
+            client.close()
+            return tweets_arr
+        except:
+            print("connection to mongo failed")
+            return []
+
 
     @staticmethod
     def get_df():
--- app/manager.py
+++ app/manager.py
@@ -1,11 +1,15 @@
-from fetcher import  Fetcher
-from processor import Processor
+from app.fetcher import  Fetcher
+from app.processor import Processor
 
 
 class Manager:
-    def __init__(self):
+    def __init__(self, flag=True):
         self.df = Fetcher.get_df()
-        self.processor =Processor()
+        if len(self.df) == 0:
+            flag = False
+        self.processor = Processor()
+
+
 
     def process(self):
         self.processor.assign_rarest_world(self.df)
@@ -13,6 +17,6 @@ class Manager:
         self.processor.assign_weapons_detected(self.df)
 
     def get_data(self):
-        return self.df.to_dict()
+        return self.df.to_dict(orient="records")
 
 
