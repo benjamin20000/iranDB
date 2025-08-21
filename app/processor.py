8b4a4dfee648f01969662530086f2f4f919b8872
benjamin <2000beni770@gmail.com>
2025-08-21 11:44:01 +0300

    add func for assign_emotion

--- app/processor.py
+++ app/processor.py
@@ -1,7 +1,7 @@
 from fetcher import  Fetcher
-
-
-
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from tqdm.auto import tqdm
+import pandas as pd
 def _count_words(tweet):
     words = tweet.split()
     count = {}
@@ -12,12 +12,31 @@ def _count_words(tweet):
             count[word] = 1
     return count
 
-def assign_rarest_world():
-    df = Fetcher.get_df()
+def assign_rarest_world(df):
     df['rarest_world'] = None
     for index in df.index:
         my_dict = _count_words(df.loc[index]["Text"])
         df.loc[index,'rarest_world'] = min(my_dict, key=my_dict.get)
+
+def _classified_emotion(tweet):
+    score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+    return score["compound"]
+
+def assign_emotion(df):
+    text_col = df["Text"]
+    tqdm.pandas()
+    scores = text_col.progress_apply(_classified_emotion) # pandas apply
+    print(scores)
+    bins = [-1, -0.5, 0.5, 1]
+    labels = ["negative", "neutral", "positive"]
+    df['sentiment'] = pd.cut(scores, bins=bins, labels=labels, right=True)
     print(df)
 
-assign_rarest_world()
+
+
+
+
+df = Fetcher.get_df()
+# assign_rarest_world(df)
+assign_emotion(df)
+# print(df)
