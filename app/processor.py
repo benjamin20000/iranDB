2cc415bca6fa72f564f0e7cbc78901137473f8e6
benjamin <2000beni770@gmail.com>
2025-08-21 12:04:55 +0300

    add func for loading blacklist

--- app/processor.py
+++ app/processor.py
@@ -2,6 +2,9 @@ from fetcher import  Fetcher
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from tqdm.auto import tqdm
 import pandas as pd
+from config import blacklist_path
+
+
 def _count_words(tweet):
     words = tweet.split()
     count = {}
@@ -11,6 +14,7 @@ def _count_words(tweet):
         else:
             count[word] = 1
     return count
+
 def _find_rarest_world(tweet):
     my_dict = _count_words(tweet)
     return min(my_dict, key=my_dict.get)
@@ -19,7 +23,6 @@ def assign_rarest_world(df):
     text_col = df["Text"]
     tqdm.pandas()
     df['rarest_world'] =  text_col.progress_apply(_find_rarest_world)
-
 
 def _classified_emotion(tweet):
     score = SentimentIntensityAnalyzer().polarity_scores(tweet)
@@ -34,10 +37,22 @@ def assign_emotion(df):
     df['sentiment'] = pd.cut(scores, bins=bins, labels=labels, right=True)
 
 
+def _load_blacklist():
+    with open(blacklist_path, "r") as file:
+        text = file.read()
+        black_list = text.split()
+        return black_list
 
+def _detected_weapons():
 
+def assign_weapons_detected(df):
+    black_list = _load_blacklist()
+    text_col = df["Text"]
+    tqdm.pandas()
+    df['rarest_world'] = text_col.progress_apply(_detected_weapons)
 
-df = Fetcher.get_df()
-assign_rarest_world(df)
-assign_emotion(df)
-print(df)
+_load_blacklist()
+# df = Fetcher.get_df()
+# assign_rarest_world(df)
+# assign_emotion(df)
+# print(df)
